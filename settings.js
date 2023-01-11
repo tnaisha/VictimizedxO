ad5b6f359850368b34a709dee85bcb38c50d035a
tnaisha <121387974+tnaisha@users.noreply.github.com>
2023-01-11 22:10:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,18 +6,18 @@ global.simpleBotname = "Anya" //⚠️ Only use normal and one work name here or
 
              //--------Owner & vcard--------\\
              global.botname = "Queen Anya" 
-             global.ownernumber = ['918811074852'] // Owner number //
-             global.vcardowner = ['918811074852'] // vcard owner number //
-             global.ownername = "3.69_Pika" // Owner name //
+             global.ownernumber = ['919098723453'] // Owner number //
+             global.vcardowner = ['919098723453'] // vcard owner number //
+             global.ownername = "Victimized xO" // Owner name //
              global.ytname = "YT: ONYXPIKA" // Yt chanel name //
              global.socialm = "𝗚𝗶𝘁𝗵𝘂𝗯 : Pika4o4" // Github or insta name //
              global.location = "India, Assam, Dibrugarh" // Location //
-             global.owner = ['918811074852','917355622763','919673141317']
-             global.ownertag = '918811074852' // Tag number //
+             global.owner = ['919098823453']
+             global.ownertag = '919098723453' // Tag number //
              global.linkz = "https://wa.me/+918811074852" // Theme url which will be displayed on whatsapp //
              global.websitex = "https://github.com/Pika4O4" // Website to be displayed //
              global.botscript = 'https://github.com/PikaBotz/Anya-pika-MD-v2' // Script link //
-             global.packname = "𝐴𝑛𝑦𝑎💫 вσᡶ 𝖻ꪗ ρỉƙꪖ" // sticker watermark packname //
+             global.packname = "𝐴𝑛𝑦𝑎💫 вσᡶ 𝖻ꪗ Victimized" // sticker watermark packname //
              global.author = "🥵3.69_ρỉƙꪖ" // sticker watermark author //
              actiontitle = '࿙⚜𝑨𝒏𝒚𝒂⚜࿚' // text who displays on the top of welcome and left message //
              global.premium = ['918811074852'] // premium numbers //
